0698a5ef5c6fa4a4782db32aca5279c0ada6c559
Gokulakrishnan-maker <gokulakrishnan756@gmail.com>
2025-09-02 23:45:56 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -39,7 +39,7 @@ const Contact = () => {
     {
       icon: MapPin,
       title: 'Service Area',
-      details: 'No 45, Rajalakshmi colony, tvs nagar, edayarpalayam, Coimbatore - 641025,'
+      details: 'No 45, Rajalakshmi colony, tvs nagar, edayarpalayam, Coimbatore - 641025',
       description: 'Local & outstation trips'
     },
     {

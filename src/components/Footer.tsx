069971ff83747adbb049f484c73bafafd6baad52
Gokulakrishnan-maker <gokulakrishnan756@gmail.com>
2025-09-08 11:37:18 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -49,7 +49,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <AtSign className="h-4 w-4 text-orange-400" />
-                <span className="text-gray-300">kovaidroptaxi38@gmail.com</span>
+                <span className="text-gray-300">1waytaxi.booking@gmail.com</span>
               </div>
               <div className="flex items-center space-x-3">
                 <MapPin className="h-4 w-4 text-orange-400" />
